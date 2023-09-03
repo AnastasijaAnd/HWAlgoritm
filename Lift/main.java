519dc520df37eda723719849bda553fea79a33f0
AnastasijaAnd <rosaamina@mail.ru>
2023-09-03 23:39:05 +0300

    lf111

--- Lift/main.java
+++ Lift/main.java
@@ -3,14 +3,12 @@ package Lift;
 public class main {
     public static void main (String[] args) throws IllegalAccessException {
         Lift lf1 = new Lift(1, 8);
-        System.out.printf("");
         lf1.move(4);
         lf1.move(6);
         lf1.move(2);
         System.out.println(lf1.toString());
         System.out.printf("");
         Lift lf2 = new Lift(12);
-        System.out.printf("");
         lf2.move(7);
         lf2.move(4);
         lf2.move(10);
