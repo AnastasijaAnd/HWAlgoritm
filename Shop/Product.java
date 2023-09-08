103bd4eac8dcda3b2c65b8b1753214b3bb44a663
AnastasijaAnd <rosaamina@mail.ru>
2023-09-08 10:40:03 +0300

    pr

--- Shop/Product.java
+++ Shop/Product.java
@@ -1,5 +1,31 @@
 package Shop;
 
 public class Product {
-    
+    private String name;
+    private double price;
+    private double rate;
+
+    public Product(String name, double price, double rate) {
+        this.name = name;
+        this.price = price;
+        this.rate = rate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public String toString() {
+        return  name +
+                ", price: " + price +
+                ", ranking: " + rate;
+    }
 }
